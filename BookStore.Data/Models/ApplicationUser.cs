3428a9b8100d9732dc19c79aa6a888269baa697c
Rajan1735 <ryadav@devfinity.io>
2023-12-29 10:57:51 +0545

    Add project files.

---
+++ BookStore.Data/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Data.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [MaxLength(10)]
+        public string Name { get; set; }
+    }
+}
---
+++ BookStoreApi/Controllers/FruitController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Api.Controllers
+{
+    [Route("api/fruits")]
+    [ApiController]
+    [Authorize]
+    public class FruitController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var fruits = await Task.FromResult(new string[] { "apple", "bananana", "kiwi" });
+            return Ok(fruits);
+        }
+
+
+        [HttpGet]
+        [Route("test")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Test()
+        {
+            var fruits = await Task.FromResult(new string[] { "apple", "bananana", "kiwi" });
+            return Ok(fruits);
+        }
+    }
+}
